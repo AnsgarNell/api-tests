eee9342a10cde7a9be45514367d358da6390196a
Ansgar Nell <fixed-term.Ansgar.Nell@de.bosch.com>
2019-06-17 15:05:09 +0200

    Added routing paths for albums and posts sections without parameters and redirection for non existing paths.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,12 +9,15 @@ import {AlbumDetailComponent} from './albums/album-detail/album-detail.component
 
 const routes: Routes = [
   { path: '', redirectTo: 'posts/start/1/limit/10', pathMatch: 'full' },
+  { path: 'posts', redirectTo: 'posts/start/1/limit/10' },
   { path: 'posts/start/:start/limit/:limit', component: PostsListComponent },
   { path: 'posts/:id', component: PostDetailComponent },
   { path: 'users', component: UsersListComponent },
   { path: 'users/:id', component: UserDetailComponent },
+  { path: 'albums', redirectTo: 'albums/start/1/limit/9' },
   { path: 'albums/start/:start/limit/:limit', component: AlbumsCardsComponent },
-  { path: 'albums/:id', component: AlbumDetailComponent }
+  { path: 'albums/:id', component: AlbumDetailComponent },
+  { path: '**', redirectTo: 'posts/start/1/limit/10' }
 ];
 
 @NgModule({
